bd3d9edfc41571d871c5fac7e86da982c82ba52b
babartos <mbartos@smu.edu>
2022-04-13 16:13:12 -0500

    form comment

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+def submit_form():
+    CSV_row = '\n' + username_input + ',' + comment_input
+    with open('comments.csv','a') as fd:
+        fd.write(CSV_row)
+
 
 st.title('Counter Example')
 if 'count' not in st.session_state:
@@ -9,3 +17,14 @@ if increment:
     st.session_state.count += 1
 
 st.write('Count = ', st.session_state.count)
+
+## comments
+username_input = st.text_input("username", "user")
+comment_input = st.text_input("write a comment", "comment")
+st.button(label="Submit", help=None, on_click=submit_form, args=None, kwargs=None)
+
+
+df = pd.read_csv("comments.csv")
+
+st.dataframe(df)  # Same as st.write(df)
+#st.line_chart(df['fare_amount'])
